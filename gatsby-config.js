45a35c2808bf7df2f47805f88b701b4c5c183f68
PERFECTIAL1\rkorin <rostyslav.korin@perfectial.com>
2023-03-13 13:47:29 +0200

    first-commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,22 +6,31 @@ module.exports = {
     title: `Ross-Store`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-datocms',
-    options: {
-      "apiToken": "f8c75606fc23fb14d3ff369c8a09c1"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    "gatsby-plugin-sass", 
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        "apiToken": "f8c75606fc23fb14d3ff369c8a09c1"
+      }
+    }, 
+    {
     resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+      options: {
+        "icon": "src/images/logo.png"
+      }
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  ]
 };
