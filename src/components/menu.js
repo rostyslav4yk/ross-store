173b81632b4fb6e8ccfa4fda9e50bafe0a731652
PERFECTIAL1\rkorin <rostyslav.korin@perfectial.com>
2023-04-06 15:44:55 +0300

    Revert "try to fix menu component"
    This reverts commit c1f3ec89ddc4d8baf2969d62ca66dcfe69be3683.

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,27 +6,44 @@ import { Link } from 'gatsby-link';
 const Menu = () => {
   const { pathname } = useLocation();
 
-  const currentLocale = !pathname.startsWith('/en/') ? 'en' : pathname.split('/')[1];
-
-  const isHomePage = pathname === `/${currentLocale}/` || pathname === `/${currentLocale}/index`;
+  const lang = pathname.startsWith("/uk/") ? "uk" : "en";
+  const isHomePage = pathname === `/${lang}/` || pathname === `/${lang}/index`;
 
   const data = useStaticQuery(graphql`
-    query MyQuery($locale: String) {
-      datoCmsMenu(locale: $locale) {
+    {
+      enMenu: datoCmsMenu(locale: "en") {
         menuItems {
           labelText
+          originalId
           destination {
             slug
-            originalId
+          }
+        }
+      }
+      ukMenu: datoCmsMenu(locale: "uk") {
+        menuItems {
+          labelText
+          originalId
+          destination {
+            slug
           }
         }
       }
     }
-  `, {
-    variables: { locale: "uk" },
-  });
+  `);
 
-  const menuItems = data.datoCmsMenu.menuItems;
+  const menuItems = lang === "en" ? data.enMenu.menuItems : data.ukMenu.menuItems;
+
+  React.useEffect(() => {
+    const links = document.querySelectorAll('.nav-link-text');
+    links.forEach(link => {
+      if (link.getAttribute('href') === pathname) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  }, [pathname]);
 
   return (
     <ul className="nav-links">
@@ -34,9 +51,9 @@ const Menu = () => {
         const linkDestination =
           menuItem.destination.slug === "index"
             ? isHomePage
-              ? `/${currentLocale}/`
-              : `${currentLocale === "en" ? "" : `/${currentLocale}`}/`
-            : `${currentLocale === "en" ? "" : `/${currentLocale}`}/${menuItem.destination.slug}`;
+              ? `/${lang}/`
+              : `${lang === "en" ? "" : `/${lang}`}/`
+            : `${lang === "en" ? "" : `/${lang}`}/${menuItem.destination.slug}`;
 
         return (
           <li key={menuItem.originalId} className="nav-link-item">
